3326b12f0f8e54e7c997ea541d6d2b923aa229bf
feynbin <feynbin@outlook.com>
2023-07-16 23:07:00 +0800

    committed changes

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * @name: hello-world
+ * @author: feynb
+ * @date: 2023-07-10 18:24
+ * @description: my first java class
+ **/
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int a = 9 + 90 * 2 + 302 * 3;
+        System.out.println(a);
+    }
+}
